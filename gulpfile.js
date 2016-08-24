4400fac4076caca46586c3803125e95dc7988bbb
Safort <a.asafort@gmail.com>
2016-08-25 01:50:44 +0300

    updated build config

--- gulpfile.js
+++ gulpfile.js
@@ -3,46 +3,17 @@
 const path = require('path');
 const gulp = require('gulp');
 const gutil = require('gulp-util');
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
 const babel = require('gulp-babel');
 const plumber = require('gulp-plumber');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const nodemon = require('gulp-nodemon');
 const Cache = require('gulp-file-cache');
 const cache = new Cache();
 
-const frontendConfig = require('./webpack-frontend.config.js');
-const devServerConfig = require('./webpack-dev-server.config.js');
-const onBuild = (done) => {
-  return (err, stats) => {
-    if (err) {
-      console.log('Error', err);
-    } else {
-      console.log(stats.toString());
-    }
 
-    if (done) {
-      done();
-    }
-  }
-};
-
-if (process.env.NODE_ENV === 'production') {
-  frontendConfig.devtool = 'cheap-module-source-map';
-} else {
-  frontendConfig.devtool = 'source-map';
-  frontendConfig.debug = true;
-}
+gulp.task('build', ['backend-prod']);
 
 
-gulp.task('default', function() {});
-
-
-gulp.task('build', ['frontend-build', 'backend-prod']);
-
-
-gulp.task('watch', ['frontend-watch', 'backend-dev'], () => {
+gulp.task('watch', ['backend-dev'], () => {
   gulp.watch('./src/backend/**/*.js', ['backend-dev']);
 
   nodemon({
@@ -65,30 +36,6 @@ gulp.task('watch', ['frontend-watch', 'backend-dev'], () => {
 });
 
 
-gulp.task('frontend-build', cb => {
-  webpack(frontendConfig).run(onBuild(cb));
-});
-
-
-gulp.task('frontend-watch', () => {
-  new WebpackDevServer(webpack(devServerConfig), {
-    publicPath: devServerConfig.output.publicPath,
-    contentBase: './app/public',
-    hot: true,
-    historyApiFallback: true,
-  }).listen(3000, 'localhost', err => {
-    if (err) {
-      throw new gutil.PluginError('webpack-dev-server', err);
-    }
-
-    gutil.log(
-      '[webpack-dev-server]',
-      'http://localhost:3000/webpack-dev-server/index.html'
-    );
-  });
-});
-
-
 gulp.task('backend-prod', () => {
   return gulp
     .src('./src/backend/**/*.js', { base: './src/backend' })
@@ -98,11 +45,11 @@ gulp.task('backend-prod', () => {
 
 
 gulp.task('backend-dev', () => {
-    return gulp
-      .src('./src/backend/**/*.js', { base: './src/backend' })
-      .pipe(cache.filter())
-      .pipe(plumber())
-      .pipe(babel())
-      .pipe(cache.cache())
-      .pipe(gulp.dest('app/'));
+  return gulp
+    .src('./src/backend/**/*.js', { base: './src/backend' })
+    .pipe(cache.filter())
+    .pipe(plumber())
+    .pipe(babel())
+    .pipe(cache.cache())
+    .pipe(gulp.dest('app/'));
 });
