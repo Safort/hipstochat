6affee0f44f8ab5f7bb13f9ffcf9b320f174c02f
Safort <a.asafort@gmail.com>
2016-08-31 20:51:45 +0300

    moved out routers to specific dir

--- src/frontend/js/containers/App/index.js
+++ src/frontend/js/containers/App/index.js
@@ -4,32 +4,12 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { browserHistory, Router } from 'react-router';
-
-import Main from '../main';
-import Signup from '../signup';
-import Search from '../search';
-import Home from '../home';
-import Pm from '../pm';
-import Profile from '../profile';
-import NoMatch from '../../components/no-match.js';
-
+import routers from '../../routers';
 import * as userActions from '../../actions/userActions';
 
-const routeConfig = {
-  path: '/',
-  component: Main,
-  indexRoute: { component: Home },
-  childRoutes: [
-    { path: '/signup', component: Signup },
-    { path: '/profile/edit', component: Profile },
-    { path: '/pm/:usename', component: Pm },
-    { path: '/search', component: Search },
-    { path: '*', component: NoMatch },
-  ],
-};
 
 const App = () => (
-  <Router history={browserHistory} routes={routeConfig} />
+  <Router history={browserHistory} routes={routers} />
 );
 
 function mapDispatchToProps(dispatch) {
---
+++ src/frontend/js/routers/index.js
@@ -0,0 +1,22 @@
+import Main from '../containers/main';
+import Signup from '../containers/signup';
+import Search from '../containers/search';
+import Home from '../containers/home';
+import Pm from '../containers/pm';
+import Profile from '../containers/profile';
+import NoMatch from '../components/no-match.js';
+
+const routes = {
+  path: '/',
+  component: Main,
+  indexRoute: { component: Home },
+  childRoutes: [
+    { path: '/signup', component: Signup },
+    { path: '/profile/edit', component: Profile },
+    { path: '/pm/:usename', component: Pm },
+    { path: '/search', component: Search },
+    { path: '*', component: NoMatch },
+  ],
+};
+
+export default routes;
