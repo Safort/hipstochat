e95f9064128e14efd033e8cba5a5181bf14661de
Safort <a.asafort@gmail.com>
2016-09-06 02:43:59 +0300

    refactor

---
+++ src/frontend/js/containers/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { browserHistory, Router } from 'react-router';
+import routers from '../routers';
+
+const App = () => <Router history={browserHistory} routes={routers} />;
+
+export default App;
--- src/frontend/js/containers/main/index.js
+++ src/frontend/js/containers/main/index.js
@@ -1,3 +1,5 @@
+import './index.css';
+
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
